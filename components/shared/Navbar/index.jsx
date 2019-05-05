96b03c8749f29e48092c799c491701f82a51e439
Julia Orduno <juliapaola97@gmail.com>
2019-05-05 13:39:23 -0500

    Fix routing

--- components/shared/Navbar/index.jsx
+++ components/shared/Navbar/index.jsx
@@ -2,49 +2,46 @@ import React, { PureComponent } from 'react';
 import { Menu } from 'semantic-ui-react';
 import { NavMenu, NavItem } from './styles';
 import { withRouter } from 'next/router';
-import Link from 'next/link';
+import { Link } from '../../../routes';
 
-class Navbar extends PureComponent {
-	state = {};
-	
-	render() {
-		const { router } = this.props;
-		const route = router.route;
+const Navbar = ( props ) => {
 
-		return (
-			<NavMenu className='top fixed' pointing secondary>
-				<Menu.Menu position='right'>
-					<Link href="/">
-						<NavItem
-							name='home'
-							className="mb-nav"
-							active={route === '/'}
-						>
-							Home
-						</NavItem>
-					</Link>
-					<Link href="/">
-						<NavItem 
-							name='map' 
-							className="mb-nav"
-							active={route === '/map'} 
-						>
-							Map
-						</NavItem>
-					</Link>
-					<Link href="/">
-						<NavItem
-							name='about'
-							className="mb-nav"
-							active={route === '/about'}
-						>
-							About
-						</NavItem>
-					</Link>
-				</Menu.Menu>
-			</NavMenu>
-		)
-	}
+	const { router } = props;
+	const route = router.route;
+
+	return (
+		<NavMenu className="top fixed" pointing secondary>
+			<Menu.Menu position="right">
+				<Link route="/">
+					<NavItem
+						name="home"
+						className="mb-nav"
+						active={route === "/"}
+					>
+						Home
+					</NavItem>
+				</Link>
+				<Link route="home">
+					<NavItem 
+						name="map" 
+						className="mb-nav"
+						active={route === "/map"} 
+					>
+						Map
+					</NavItem>
+				</Link>
+				<Link route="home">
+					<NavItem
+						name="about"
+						className="mb-nav"
+						active={route === "/about"}
+					>
+						About
+					</NavItem>
+				</Link>
+			</Menu.Menu>
+		</NavMenu>
+	)
 };
 
 Navbar.displayName = 'Navbar';
